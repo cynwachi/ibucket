40b60dda777750c3ce8a433ddd97539ec081afb6
cynwachi <christinenwachi@gmail.com>
2020-11-08 10:48:24 -0600

    setting up django

--- bucketlist/models.py
+++ bucketlist/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+#Reads and writes data 
 # Create your models here.
+
+class MyWishName(models.Model):
+   # """ Atypical class defiing a model, derived from the MOdel class."""
+    
+    #Fields
+    input_your_wish = models.CharField(max_length=30, help_text='Enter your wish')
+    #
+    
+    #METADATA
+    class Meta
+        ordering =['-input_your_wish']
+    
+    #Methods
+    
+    def get_absolute_url(self):
+        # """Returns the url to access a particular instance of MyModelName."""
+        return reverse('model-detail-view', args=[str(self.id)])
+    
+    def __str__(self):
+        #"""String for representing the MyWishName object (in Admin site etc."""
+        return self.input_your_wish
--- bucketlist/views.py
+++ bucketlist/views.py
@@ -1,3 +1,3 @@
 from django.shortcuts import render
-
+#receives messages from urls.py and models.py and from the Template loacated at filename.html
 # Create your views here.
