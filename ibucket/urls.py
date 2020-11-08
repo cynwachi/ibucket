82298ed6dfe72ada554b2ee2dc17b61b06173756
cynwachi <christinenwachi@gmail.com>
2020-11-08 11:03:22 -0600

    working on views

--- ibucket/urls.py
+++ ibucket/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
+urlpatterns += [
+    path('admin/', include('admin.site.urls')),
+]
+
+urlpatterns = [andpath('' views.index, name'index'),
 ]
