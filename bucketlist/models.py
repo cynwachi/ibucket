c53a9b146fec683c52277778f5d11f9c8a85a763
cynwachi <christinenwachi@gmail.com>
2020-11-10 22:26:11 -0600

    views

--- bucketlist/models.py
+++ bucketlist/models.py
@@ -1,22 +1,58 @@
 from django.db import models
+from django.core.validators import RegexValidator
+from django.db.models.fields import DateTimeField, TextField
+from localflavor.us.models import USSteField, USZipCodeField
 
 #from metadata.mixins import MetadataMixin
 #Reads and writes data 
 # Create your models here.
 
-class MyPrayerName(models.Model):
-   input_your_prayer = models.CharField(max_length=30, help_text='Enter your wish')
-    
-    
+class User(models.Model):
+   phone_regex=RegexValidator(
+       regex=r'^\+?\d{10$',
+       message="Phone number must be entered in the format: '+9999999999'.")
+   
+   name =models.CharField(max_length=255)
+   email = models.EmailField(null=True, blank=True)
+   phone_number = models.CharField(max_length=11,
+                                   validators=[phone_regex],
+                                   null=True,
+                                   blank=True)
+   address_1 = models.CharField(max_length=255, null=True, blank=True)
+   address_2 = models.CharField(max_length=255, null=True, blank=True)
+   city = models.CharField(max_length=255, null=True, blank=True)
+   state = USStateField(null=true,blank=True)
+   zip_code = USZipCodeField(null=true,blank=True)
+   birthday = models.CharField(null=True, blank=True)
+   
+   def __str__(self):
+       return self.name
+
+class Play(models.Model):
+    text = models.TextField(max_length=150)
+    date_time = models.DateTimeField(auto_now_add=False)
+    user = models.ForeignKey(to="User", on_delete=models.CASCADE, related_name="user randomness"
+
     #METADATA
     #class Meta(MetadataMixin, models
         #username = models.CharField(max_length=150)
     #Methods
     
     def get_absolute_url(self):
-        # """Returns the url to access a particular instance of MyPrayerName."""
+        # """Returns the url to access a particular instance of PlayPray."""
         return reverse('model-detail-view', args=[str(self.id)])
     
     def __str__(self):
         #"""String for representing the MyWishName object (in Admin site etc."""
-        return self.input_your_wish
+        return self.text
+    
+class Pray(models.Model):
+        text = models.TextField(max_length=150), help_text='Enter your Play')
+    
+    def get_absolute_url(self):
+        # """Returns the url to access a particular instance of Pray."""
+        return reverse('model-detail-view', args=[str(self.id)])
+    
+    def __str__(self):
+        #"""String for representing the Pray object (in Admin site etc."""
+        return self.text
--- bucketlist/views.py
+++ bucketlist/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import User
+from .forms import UserForm, PlayForm, PrayForm, forms
 #receives messages from urls.py and models.py and from the Template loacated at filename.html
 # Create your views here.
+
+def list_users(request):
+    users = User.objects.all()
+    return render (request, "users/list_users.html",
+                   {"users": users})
+    
+def add_user(request):
+    if request.method == 'GET':
+        form = UserForm()
+    else:
+        form = UserForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(to='list_contacts')
+        
+    return render(request, "users/add_user.html", {"form": form}):
+
+def add_pray(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    if request.method == "POST":
+      form = NoteForm(data=request.POST)
+      if form.is_valid():
+        note = forms.save(commit=False)
+        note.contact=contact
+    return redirect(to="user_detail", pk=pk)
+
+def edit_user(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    if request.method == 'GET':
+        form = UserForm(instance=user)
+    else:
+        form = UserForm(instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect(to='list_users')
+    
+    return render(request, "users/edit_user.html", {
+        "form": form,
+        "user": user
+        
+    })
+    
+def delete_user(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    if request.method =='POST':
+        user.delete()
+        return redirect(to='list_users'
+                        
+    return render (request, "users/delete_user.html",
+                   {"user": user})
+)
