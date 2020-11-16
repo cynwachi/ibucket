22e0bd2ba020f58fa65c5061e25bc1cbcfca8568
cynwachi <christinenwachi@gmail.com>
2020-11-15 19:39:24 -0600

    changed tree

--- migrations/bucketlist/models.py
+++ migrations/bucketlist/models.py
@@ -30,7 +30,7 @@ class User(models.Model):
        return self.name
 
 class Play(models.Model):
-    text = models.TextField(max_length=150, help_text= "Enter Your Play")
+    text = models.TextField(max_length=150,)
     date_time = models.DateTimeField(auto_now_add=False)
     user = models.ForeignKey(to="User", on_delete=models.CASCADE, related_name="users")
 
@@ -39,9 +39,9 @@ class Play(models.Model):
         #username = models.CharField(max_length=150)
     #Methods
     
-    def get_absolute_url(self):
+    #def get_absolute_url(self):
         # """Returns the url to access a particular instance."""
-        return reverse('model-detail-view', args=[str(self.id)])
+    #    return reverse('model-detail-view', args=[str(self.id)])
     
     def __str__(self):
         #"""String for representing the MyWishName object (in Admin site etc. used any place you "RENDER"    """
@@ -50,9 +50,9 @@ class Play(models.Model):
 class Pray(models.Model):
         text = models.TextField(max_length=150)
     
-    def get_absolute_url(self):
+    #def get_absolute_url(self):
         # """Returns the url to access a particular instance of Pray."""
-        return reverse('model-detail-view', args=[str(self.id)])
+    #    return reverse('model-detail-view', args=[str(self.id)])
     
     def __str__(self):
         #"""String for representing the Pray object (in Admin site etc."""
