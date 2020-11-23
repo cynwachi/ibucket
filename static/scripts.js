7e241ad745f4d2877fe0b54a74ace8413689f1a0
cynwachi <christinenwachi@gmail.com>
2020-11-23 07:14:55 -0600

    added to setting.py STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]

--- static/scripts.js
+++ static/scripts.js
@@ -2,31 +2,29 @@ let plays = document.getElementById("plays");
 let prays = document.getElementById("prays");
 let bucketsList = document.getElementById("bucketsList");
 
-
-let circleMaxSize = 10; 
-let circleMinSize = 7;
 let circleMaxSize = 80; 
 let circleMinSize = 60;
 
 let praysArray = ["hello","Yoyo"];
 let playsArray = ["konnichiwa", "Jeff"];
-for (let pray of praysArray) {
+let elemIDNames = [];
 
-}
-
-//Objects
-for (let play of playsArray){                           //Makes a loot for each play bucket their is
-for (let play of playsArray){                           //Makes a loop for each play bucket their is
-    let elemID = playsArray.indexOf(play);              //Makes a unique ID for the play object
+//Objects                        
+ //Makes a loop for each play bucket their is
+for (let pray of praysArray){                           //Makes a loop for each play bucket their is
+    elemID = playsArray.indexOf(play);              //Makes a unique ID for the play object
     let newElement = document.createElement('div');     //Creates an object in the page and assigns it to a variable
-    newElement.classList.add('plays');                  //Gives the object the class name plays
+    newElement.classList.add('plays');  
+    newElement.id = `prays${elemID}`;
+    bucketsList.appendChild(newElement);
+    
 for (let play of playsArray){                           //Makes a loot for each
     let elementName = document.createElement('div');    //Creates an object in the page and assigns it to a variable
     elementName.classList.add('name');                  //Gives the object the class name 'name'
     elementName.innerText = play;
     newElement.appendChild(elementName);  //Adds a name to the play object
-    newElement.appendChild(elementName);                //Adds a name to the play object
-}
+    newElement.appendChild(elementName);                //Adds a name to the pray object
+}}
 
 //Randomizes the size of the circle 
 var buckets = document.getElementById('bucketsList').children;
@@ -39,5 +37,4 @@ for(let bucketChild of buckets) {
     bucketChild.style.width =  randomSize + 'px';
     bucketChild.style.height = randomSize + 'px';
     //bucketChild.style.left = innerWidth + 'px'
- }}
-}
+}}
