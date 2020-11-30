f2b50e47e465a6067597a13d16422222f379c683
Reuel Belt <reuelbelt@gmail.com>
2020-11-30 13:36:01 -0800

    updated font and pray play page

--- ibucket/asgi.py
+++ ibucket/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for iBe project.
+ASGI config for ibucket project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iBe.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ibucket.settings')
 
 application = get_asgi_application()
--- ibucket/urls.py
+++ ibucket/urls.py
@@ -1,7 +1,7 @@
-"""iBe URL Configuration
+"""ibucket URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,21 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf import settings
-from django.urls import include, path
-from core import views as page_views
+from django.urls import path, include
+from bucketlist import views as page_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', page_views.viewHompage, name='homepage'),
-    # path('', page_views.addBucket, name='addBucket'),
+    path('', page_views.viewHompage, name='home'),
+    path('bucket/add/', page_views.addBucket, name='addBucket'),
 ]
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-
-        # For django versions before 2.0:
-        # url(r'^__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
